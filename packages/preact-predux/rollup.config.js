c0185a5c475b910e79c2f01c0ab0cc332702267e
CalmDownVal <vaclav.lahuta@gmail.com>
2021-02-19 00:39:48 +0100

    configure rollup builds

---
+++ packages/preact-predux/rollup.config.js
@@ -0,0 +1,44 @@
+import eslint from '@rollup/plugin-eslint';
+import typescript from '@rollup/plugin-typescript';
+import definitions from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+	{
+		input: './src/index.ts',
+		output: [
+			{
+				file: './build/index.cjs.min.js',
+				format: 'cjs'
+			},
+			{
+				file: './build/index.esm.min.js',
+				format: 'esm'
+			}
+		],
+		external: [
+			'@calmdownval/signal',
+			'@calmdownval/predux',
+			'preact'
+		],
+		plugins: [
+			eslint(),
+			typescript(),
+			terser({
+				output: {
+					comments: false
+				}
+			})
+		]
+	},
+	{
+		input: './src/index.ts',
+		output: {
+			file: './build/index.d.ts',
+			format: 'es'
+		},
+		plugins: [
+			definitions()
+		]
+	}
+];
